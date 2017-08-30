4aed8252d86d8e4cf1b884a86d52fca7f98c3659
Ciarán O’Mara <Ciaran.OMara@utas.edu.au>
2017-08-30 15:48:17 +1000

    Republish vendor files

--- config/debugbar.php
+++ config/debugbar.php
@@ -12,7 +12,7 @@ return [
      |
      */
 
-    'enabled' => env('DEBUGBAR_ENABLED', null),
+    'enabled' => env('DEBUGBAR_ENABLED', env('APP_DEBUG', false)),
 
     /*
      |--------------------------------------------------------------------------
@@ -110,12 +110,13 @@ return [
         'session'         => true,  // Display session data
         'symfony_request' => true,  // Only one can be enabled..
         'mail'            => true,  // Catch mail messages
-        'laravel'         => true, // Laravel version and environment
-        'events'          => true, // All events fired
+        'laravel'         => false, // Laravel version and environment
+        'events'          => false, // All events fired
         'default_request' => false, // Regular or special Symfony request logger
         'logs'            => false, // Add the latest log messages
         'files'           => false, // Show the included files
         'config'          => false, // Display config settings
+        'cache'           => false, // Display cache events
     ],
 
     /*
@@ -152,6 +153,9 @@ return [
         ],
         'logs' => [
             'file' => null
+        ],
+        'cache' => [
+            'values' => true // collect cache values
         ],
     ],
 
